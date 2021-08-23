a5b3dc8501233a0410b85c072eba0149432040ca
Alisson Bomfim <alissonbomfimsilva@outlook.com>
2021-08-23 16:56:02 -0300

    Criando classe Link

--- PBL1_AP2/src/modulo/Link.java
+++ PBL1_AP2/src/modulo/Link.java
@@ -1,5 +1,24 @@
 package modulo;
 
 class Link {
-
+	private Object data;
+	private Link proximo;
+	public Object getData() {
+		return data;
+	}
+	public void setData(Object data) {
+		this.data = data;
+	}
+	public Link getProximo() {
+		return proximo;
+	}
+	public void setProximo(Link proximo) {
+		this.proximo = proximo;
+	} 
+	
+	public Link(Object data){
+		this.data = data;
+	}
+	
 }
+
