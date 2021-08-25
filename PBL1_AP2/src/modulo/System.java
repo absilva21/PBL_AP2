daaca5dd3a8999c0f05ceeea67b4a21944c751be
Alisson Bomfim <alissonbomfimsilva@outlook.com>
2021-08-25 14:49:13 -0300

    Construindo a classe System

--- PBL1_AP2/src/modulo/System.java
+++ PBL1_AP2/src/modulo/System.java
@@ -1,5 +1,20 @@
 package modulo;
 
 class System {
-
+	LinkList clientes;
+	LinkList Cardapio;
+	public LinkList getClientes() {
+		return clientes;
+	}
+	public void setClientes(LinkList clientes) {
+		this.clientes = clientes;
+	}
+	public LinkList getCardapio() {
+		return Cardapio;
+	}
+	public void setCardapio(LinkList cardapio) {
+		Cardapio = cardapio;
+	}
+	
+	
 }
